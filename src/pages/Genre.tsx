f737e8167c9a3dca889e4956e4a45769573d1de7
Islam Mušić <islam.music@scv.si>
2025-04-24 11:44:05 +0200

    fe genre

--- src/pages/Genre.tsx
+++ src/pages/Genre.tsx
@@ -1,12 +1,32 @@
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Genre = () => {
-  return (
+    const [genres, setGenres] = useState([]);
+
+    const getData = async () => {
+        const res = await axios.get("http://localhost:3000/genres");
+        if (res.status === 200) {
+            setGenres(res.data);
+        }
+    }
+
+    useEffect(() =>  {
+        getData();
+    }, []);
+
+    return (
       <>
         <Header />
         <main>
             <h1>Žanri</h1>
+            <ul>
+                {genres.map((genre) => (
+                    <li key={genre.id}>{genre.name}</li>
+                ))}
+            </ul>
         </main>
         <Footer />
       </>
